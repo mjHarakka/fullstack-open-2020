a85fbdce5da35707ffe0de92087144dfcfe0eedb
Mikko Harakka <mj.harakka@gmail.com>
2020-04-08 22:08:15 +0300

    commit before refactoring

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,91 +1,181 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+
+const Number = (props) => {
+  const name = props.person.name
+  const number = props.person.number
+  return (
+    <li>{name} {number}</li>
+  )
+}
+
+const Persons = (props) => {
+  const filterValue = props.filterValue
+  const persons = props.persons
+
+  if (filterValue.length > 0) {
+    const copy = []
+
+    for (let i = 0; i < persons.length; i++) {
+
+      if (persons[i].name.toLowerCase().includes(filterValue.toLowerCase())) {
+        copy.push(persons[i])
+      }
+    }
+    return (copy.map(copy =>
+      <Number
+        key={copy.name}
+        person={copy}
+      />))
+
+  } else {
+    return (persons.map(person =>
+      <Number
+        key={person.name}
+        person={person}
+      />))
+  }
+}
+
+const PersonForm = (props) => {
+  const addNumber = props.addNumber
+  const newName = props.newName
+  const handleNameChange = props.handleNameChange
+  const newNumber = props.newNumber
+  const handleNumberChange = props.handleNumberChange
+
+  return (
+    <form onSubmit={addNumber}>
+      nimi:
+        <input
+        value={newName}
+        onChange={handleNameChange}
+      />
+      <br />
+      numero:<input
+        value={newNumber}
+        onChange={handleNumberChange}
+      />
+      <br />
+      <button type="submit">lisää</button>
+    </form>
+  )
+}
 
 const Filter = (props) => {
-    return (
-        <div>
-            filter shown with 
-            <input value={props.filterValue} onChange={props.handleFiltering}/>
-        </div>
-    )
+  return (
+    <input
+      value={props.filterValue}
+      onChange={props.handleFiltering}
+    />
+  )
 }
 
 const App = () => {
-    const [ persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
 
-    const [ newName, setNewName ] = useState('')
-    const [ newNumber, setNewNumber ] = useState('')
-    const [ filterValue, setNewFilterValue] = useState('')
-    const [ filteredPersons, setFilteredPersons] = useState([])
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterValue, setNewFilterValue] = useState('')
 
-    const addPerson = (event) => {
-        event.preventDefault()
-        
-        const personObject = {
-            id: persons.length + 1,
-            name: newName,
-            number: newNumber
-        }
-        
-        if (persons.filter(e => e.name === newName).length > 0) {
-            alert(`${newName} is already added to phonebook`)
-            setNewName('')
-        } else {
-            console.log('button clicked', event.target)
-            setPersons(persons.concat(personObject))
-            setNewName('')
-        }
-        
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
+
+
+  const addNumber = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
     }
 
-    const handleFiltering = (event) => {
-        setNewFilterValue(event.target.value)
-        setFilteredPersons(persons.filter(person => person.name.indexOf(filterValue)))
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
+
+    let i = 0
+
+    while (i < persons.length) {
+      if (persons[i].name === newName) {
+        break
+      }
+      i++
     }
 
-    const handleNameChange = (event) => {
-        setNewName(event.target.value)
+    if (newName.length === 0) {
+      alert("Et voi syöttää tyhjää kenttää")
+    } else if (i < persons.length) {
+      alert(newName + " on jo luettelossa")
     }
 
-    const handleNumberChange = (event) => {
-        setNewNumber(event.target.value)
+    if (i === persons.length && newName.length !== 0) {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+    } else {
+      setNewName('')
+      setNewNumber('')
     }
 
-    console.log(filterValue)
+  }
 
-    return (
-        <div>
-            <h2>Phonebook</h2>
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
-            <Filter filterValue={filterValue} handleFiltering={handleFiltering}/>
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
 
-            <h2>Add a new</h2>
+  const handleFiltering = (event) => {
+    console.log(event.target.value)
+    setNewFilterValue(event.target.value)
+  }
 
-            <form onSubmit={addPerson}>
-            <div>
-                name: <input value={newName} onChange={handleNameChange}/>
-            </div>
-            <div>
-                number: <input value={newNumber} onChange={handleNumberChange} />
-            </div>
+  return (
+    <div>
+      <h1>Puhelinluettelo</h1>
+      rajaa näytettäviä:
+      <Filter
+        filterValue={filterValue}
+        handleFiltering={handleFiltering}
+      />
 
-            <div>
-                <button type="submit">add</button>
-            </div>
-            </form>
-            <h2>Numbers</h2>
-            <ul>
-                {filteredPersons.map((person) => <li key={person.name}> {person.name} {person.number}</li>)}
-            </ul>
-            
-           
-        </div>
-    )
+      <h2>Lisää uusi</h2>
+      <PersonForm
+        addNumber={addNumber}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h2>Numerot</h2>
+      <Persons filterValue={filterValue} persons={persons} />
+
+    </div>
+  )
 
 }
 
 export default App
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
